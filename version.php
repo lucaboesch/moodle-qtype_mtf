c24ed0eac36329478fd3fc07f41ead1d0a8d4af3
Amr <amr.hourani@id.ethz.ch>
2017-01-27 15:35:39 +0100

    latest version

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_mtf';
-$plugin->version = 2017012901;
+$plugin->version = 2017012902;
 $plugin->requires = 2013111904; // Moodle >=2.6.4.
 
 $plugin->maturity = MATURITY_STABLE;
