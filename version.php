b48382cfce146cdac82ec610aba185f70c08f339
Nexterday <nexterday@users.noreply.github.com>
2024-10-01 08:53:00 +0200

    Update version.php

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_mtf';
-$plugin->version = 2023111300;
+$plugin->version = 2024100100;
 $plugin->requires = 2021050100; // Moodle >=3.11+.
 
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.4';
+$plugin->release = '4.5';
